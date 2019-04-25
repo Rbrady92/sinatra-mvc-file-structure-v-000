da3607c672c1186ddd4eb9628cc3f91987c7debd
Ryan Brady <ryanbrady369@gmail.com>
2019-04-25 21:26:31 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,4 @@
 class ApplicationController < Sinatra::Base
-  attr_accessor :name, :breed, :age
-
-  def initialize(name, breed, age)
-    @name = name
-    @breed = breed
-    @age = age
-  end
-
   configure do
   	set :views, "app/views"
   	set :public_dir, "public"
